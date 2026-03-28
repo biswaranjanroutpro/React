6e28cc79e907fe56c707a333a296f41836894409
Biswaranjan Rout <biswaranjanrout.pro@gmail.com>
2026-03-28 22:01:14 +0530

    feat(card): implement the whole logic

--- card-project/src/components/Card.jsx
+++ card-project/src/components/Card.jsx
@@ -1,8 +1,31 @@
-function Card() {
+import { Bookmark } from "lucide-react";
+
+function Card(props) {
 
     return (
         <div className="card">
-            
+            <div className="top">
+                <img src={props.brandLogo} alt="" />
+                <button>Save <Bookmark size={13}/></button>
+            </div>
+
+            <div className="center">
+                <h3>{props.companyName} <span>{props.datePosted}</span></h3>
+                <h2>{props.post}</h2>
+                <div className="tag">
+                    <h4>{props.tag1}</h4>
+                    <h4>{props.tag2}</h4>
+                </div>
+            </div>
+
+            <div className="bottom">
+                <div>
+                    <h3>{props.pay}</h3>
+                    <p>{props.location}</p>
+                </div>
+                <button>Apply Now</button>
+            </div>
+
         </div>
     );
 }
